5fe514d93a0a0448aaff4d59603baec0bd247630
Oguzhan Tekbas <otekbas@gayebilisim.com>
2022-05-11 08:49:01 +0300

    rev

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,15 +1,17 @@
-import express from 'express'
-import { getPosts, createPost, getSinglePost } from '../controllers/posts.js'
-
+import express from "express";
+import {
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost,
+  getSinglePost,
+} from "../controllers/posts.js";
 const router = express.Router();
 
-//http://localhost:5000/posts
-//GET, POST, DELETE, UPDATE, PATCH
+router.get("/", getPosts);
+router.get("/:id", getSinglePost);
+router.post("/", createPost);
+router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
 
-router.get("/", getPosts)
-router.get("/:id", getSinglePost)
-router.post("/", createPost)
-
-
-
-export default router
+export default router;
